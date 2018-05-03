9e54e0e8b7406420117f65e4f74add00a1e7b6dd
Rishikesh Jadhav <Rishikesh.Jadhav@nitorinfotech.net>
2018-05-03 18:12:34 +0530

    Integrated Angular 2 Joyride directive

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { JoyrideService } from 'angular2-joyride';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+
+  constructor(private readonly joyrideService: JoyrideService) {
+
+  }
+
+  onClick() {
+    this.joyrideService.startTour();
+  }
+
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { JoyrideModule } from 'angular2-joyride';
+
 
 import { AppComponent } from './app.component';
 
@@ -10,6 +12,7 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
+    JoyrideModule,
     BrowserModule
   ],
   providers: [],
